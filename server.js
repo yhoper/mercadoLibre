29f1ff6def7a5055ccd67ed5b539f9783ff9d749
yhoper <moisevicenteperez@gmail.com>
2022-12-02 12:08:12 -0300

    updated backend

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const swaggerUI = require("swagger-ui-express");
-const swaggerJsdoc = require("swagger-jsdoc");
-const swaggerConfig = require("./documentation/swagger.config.json");
-const swaggerJSDoc = require("swagger-jsdoc");
-require("dotenv").config();
+const cors = require("cors");
+// const dotenv = require("dotenv");
+// const swaggerUI = require("swagger-ui-express");
+// const swaggerJsdoc = require("swagger-jsdoc");
+// const swaggerConfig = require("./documentation/swagger.config.json");
+// const swaggerJSDoc = require("swagger-jsdoc");
+// require("dotenv").config();
 
-//app-server
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-//middelwares-server
-app.use(express.json({ limit: "2mb" })); //le damos un limite a la respuesta de la data
-app.use(express.urlencoded({ extended: false })); //Con esto todos tus EndPoints sólo van a trabajar con json
-const swaggerDocs = swaggerJSDoc(swaggerConfig);
-app.use(
-  "/api/docs",
-  swaggerUI.serve,
-  swaggerUI.setup(swaggerDocs, { explorer: true })
-); //Ruta para swagger , el explorer es un search para la docuntación.
-app.use(cors());
+// app.use(function (req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+//   });
 
-//routes middelwares-fs Option 2 (Optime)
+//app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
+// app.use(express.json({ limit: "2mb" })); //le damos un limite a la respuesta de la data
+// app.use(express.urlencoded({ extended: false })); //Con esto todos tus EndPoints sólo van a trabajar con json
+// const swaggerDocs = swaggerJSDoc(swaggerConfig);
+// app.use(
+//   "/api/docs",
+//   swaggerUI.serve,
+//   swaggerUI.setup(swaggerDocs, { explorer: true })
+// ); //Ruta para swagger , el explorer es un search para la docuntación.
+
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 //port
--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,6 +1,60 @@
+const axios = require("axios");
+const { Namespace } = require("socket.io");
+
 exports.listAll = async (req, res) => {
   try {
-    res.send(req.params);
+    const textToShearing = req.params.search;
+    const itemsRequest = await axios(
+      `https://api.mercadolibre.com/sites/MLA/search?q=${textToShearing}&limit=20`
+    );
+
+    const allItem = await itemsRequest?.data;
+
+    const allCategories = await [];
+    const nuevaFuncion = async () => {
+      allItem.available_filters.reduce((acumulador, valorActual) => {
+        valorActual.id === "category"
+          ? valorActual.values.reduce((acum2, valorAct2) =>
+              allCategories.push(valorAct2)
+            )
+          : [];
+      }, []);
+    };
+    const getNameCategory = (category_id) => {
+      let nameCat = allCategories.find((it) => it.id === category_id);
+      let item = nameCat?.name != null ? nameCat.name : "no_category";
+      return [item];
+    };
+
+    let dd = await nuevaFuncion();
+    //console.log(allCategories);
+
+    const items = allItem.results.map((item) => ({
+      author: {
+        name: "config.firstName",
+        lastname: "config.lastname",
+      },
+      categories: getNameCategory(item.category_id),
+      items: [
+        {
+          id: item.id,
+          title: item.title,
+          price: {
+            currency: item.currency_id,
+            amount: Math.trunc(item.price),
+            decimals: item.price - Math.trunc(item.price),
+          },
+          picture: item.thumbnail,
+          condition: item.condition,
+          free_shipping: item.shipping.free_shipping,
+          address: item.address.state_name,
+        },
+      ],
+    }));
+
+    res.json(items);
+    //res.json(await items);
+    //res.json(await itemsRequest.data.results);
   } catch (error) {
     console.log(error);
     res.status(400).send("Search failed");
